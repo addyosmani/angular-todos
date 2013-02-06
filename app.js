35a34022218d9fa6bead9144778513f80e7a42f8
Dyan Benares <dyan.benares@sleepygiant.com>
2013-02-05 17:33:20 -0800

    Stored values using HTML5 Localstorage

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-function TodoCtrl($scope) {
-    $scope.todos = [
-        {text:'AngularJS Tutorial', done:false},
-        {text:'Read AngularJS Documentation', done:false},
-        {text:'Watch example videos', done:false},
-    ];
+var DEFAULT_TODOS = [
+    {text:'Watch example videos', done:false},
+    {text:'AngularJS Tutorial', done:false},
+    {text:'Read AngularJS Documentation', done:false},
+];
+var TODO_KEY = 'todos_';
 
-    // Try local storage
-    
+if (!localStorage[TODO_KEY]) {
+  localStorage[TODO_KEY] = JSON.stringify(DEFAULT_TODOS);
+}
+
+function TodoCtrl($scope) {
+    var todos = localStorage[TODO_KEY];
+    todos = todos ? JSON.parse(todos) : DEFAULT_TODOS;
+    $scope.todos = todos;
+
+    $scope.storeTodos = function () {
+        // Store updated todo list locally
+        localStorage[TODO_KEY] = JSON.stringify($scope.todos);
+    };
+
     $scope.getTotalTodos = function () {
         return $scope.todos.length;
     };
@@ -15,12 +27,26 @@ function TodoCtrl($scope) {
         $scope.todos = _.filter($scope.todos, function(todo){
             return !todo.done;
         });
+        $scope.storeTodos();
     };
 
     $scope.addTodo = function () {
+        // Add item to todo list
         $scope.todos.push({text:$scope.formTodoText, done:false});
         $scope.formTodoText = '';
+        $scope.storeTodos();
     };
+
+
+
+    $scope.sortTodos = function () {
+        var sortedtodoList = $scope.todos.sort( function(a, b) {
+            return a.text - b.text;
+        })
+        console.log(sortedtodoList)
+    }
+
+
 }
 
 
